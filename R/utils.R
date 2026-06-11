a1d57157a776bafc54a1f8c5979a2ee118545e2b
eric-pedersen <eric.pedersen@concordia.ca>
2026-06-11 18:37:29 -0400

    added helper function to download template

--- R/utils.R
+++ R/utils.R
@@ -75,3 +75,39 @@ load_gps_file <- function(file_path, type =  c("waypoints", "tracks")){
 
   gps_data
 }
+
+
+#' Downloads the most recent template file to use for data entry
+#'
+#' @param folder_path the path of the folder to download the template to.
+#' @export download_current_template
+download_current_template <- function(folder_path) {
+
+  template_file <- system.file("extdata", "LTMFTN_template_datasheet.xlsx", package = "LTMFNpackage")
+
+  if(file.exists(file.path(folder_path, "LTMFTN_template_datasheet.xlsx"))){
+    stop("Template file already exists at selected location. Delete old template or choose a new folder before proceeding")
+}
+  current_version <- readxl::read_excel(template_file,sheet = "entry_metadata")$template_version
+
+  message(paste0(
+  "You will be downloading version ",
+  current_version,
+  "of the template. It will be added to folder ",
+  folder_path,
+  ". Make sure that this is where you want the template, and that you have installed the most recent",
+  " version of the LTMFNpackage from github before downloading a new template sheet."
+  ))
+
+  user_decision <- readline(prompt = "Would you like to proceed? (enter Y or N): ")
+
+  if(user_decision %in% c("n","N") ){
+    return(FALSE)
+  } else if (user_decision %in% c("y", "Y")) {
+    file.copy(from = template_file, to = folder_path, overwrite = FALSE)
+    return(TRUE)
+  } else {
+    stop("Not a valid response. Template download not completed")
+  }
+}
+
